970ddac6fdad177d5512632d0483e3d41599e132
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-12 09:19:43 +1000

    Start working on Stage 2 - switching to protected mode

--- src/boot_sector.s
+++ src/boot_sector.s
@@ -9,6 +9,24 @@ __start:
     mov bx, hello_msg
     call print_string
 
+    mov si, disk_address_packet
+    mov ah, 0x42 ; BIOS "extended read" function
+    mov dl, 0x80 ; drive number
+    int 0x13 ; BIOS disk services
+    jc error_reading_disk
+
+ignore_disk_read_error:
+    SND_STAGE_ADDR equ (BOOT_LOAD_ADDR + SECTOR_SIZE)
+    jmp 0:SND_STAGE_ADDR
+
+error_reading_disk:
+    ;; we accept reading fewer sectors than requested
+    cmp word [dap_sectors_num], READ_SECTORS_NUM
+    jle ignore_disk_read_error
+
+    mov bx, error_reading_disk_msg
+    call print_string
+
 end:
     hlt
     jmp end
@@ -33,4 +51,18 @@ print_string_return:
     popa
     ret
 
+    align 4
+disk_address_packet:
+    db 0x10 ; size of packet
+    db 0 ; reserved, always 0
+dap_sectors_num:
+    dw READ_SECTORS_NUM ; number of sectors read
+    dd (BOOT_LOAD_ADDR + SECTOR_SIZE) ; destination address
+    dq 1 ; sector to start at (0 is the boot sector)
+
+READ_SECTORS_NUM equ 64
+BOOT_LOAD_ADDR equ 0x7c00
+SECTOR_SIZE equ 512
+
 hello_msg: db "Hello, world!", 0
+error_reading_disk_msg: db "Error: failed to read disk with 0x13/ah=0x42", 13, 10, 0
